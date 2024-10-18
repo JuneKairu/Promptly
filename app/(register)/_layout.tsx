26cd67908b36a5f38858b5a0166711c1fabdd627
Jal Devecais <chiyokogaming02@gmail.com>
2024-10-18 22:41:18 +0800

    asd

--- app/(register)/_layout.tsx
+++ app/(register)/_layout.tsx
@@ -14,7 +14,7 @@ export default function HomeLayout() {
       }}
     >
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="login" options={{ headerShown: false }} />
+      <Stack.Screen name="signup" options={{ headerShown: false }} />
       {/* <Stack.Screen name="signup" options={{ headerShown: false }} /> */}
     </Stack>
   );
