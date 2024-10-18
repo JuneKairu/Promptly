c2de07f301c197467175c13d493587d0665539f2
Jal Devecais <chiyokogaming02@gmail.com>
2024-10-19 00:45:29 +0800

    aaddd

--- app/(register)/index.tsx
+++ app/(register)/index.tsx
@@ -1,11 +1,12 @@
 import { View, Text, StyleSheet,Image } from "react-native";
 import { Link } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
+import safeareaviewbg from "@/safeareaview";
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={safeareaviewbg.safeareaviewbg}>
       <View style={styles.container}>
-        <View style={styles.ImageContainer}>
+        <View style={{}}>
           <Image source={require("../../assets/images/t")} />
         </View>
 
---
+++ safeareaview.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+import { Dimensions } from "react-native";
+
+
+
+const styles = StyleSheet.create({
+  safeareaviewbg:{
+
+    backgroundColor:'#fff'
+  }
+});
+
+export default styles;
+
