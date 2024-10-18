4a0332517b71376d3f4274c1161b406fb7b6da1b
Jal Devecais <146471877+Poorsaken@users.noreply.github.com>
2024-10-18 22:25:29 +0800

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDmRvZsRzpaBXXncmt-BJ3qDu-8lntBaog",
+  authDomain: "promptly-d20cb.firebaseapp.com",
+  projectId: "promptly-d20cb",
+  storageBucket: "promptly-d20cb.appspot.com",
+  messagingSenderId: "669976616506",
+  appId: "1:669976616506:web:54973ea2bf3d8b5d579f03",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ metro.config.js
@@ -0,0 +1,9 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const defaultconfig = getDefaultConfig(__dirname);
+defaultconfig.resolver.assetExts.push('cjs');
+
+
+module.exports = defaultconfig;
