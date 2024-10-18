59862d348675815e4dca2072b04c9d2970c2a2c8
Jal Devecais <chiyokogaming02@gmail.com>
2024-10-19 00:47:44 +0800

    asdfg

--- app/(register)/index.tsx
+++ app/(register)/index.tsx
@@ -1,10 +1,10 @@
 import { View, Text, StyleSheet,Image } from "react-native";
 import { Link } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
-import safeareaviewbg from "@/safeareaview";
+
 export default function HomeScreen() {
   return (
-    <SafeAreaView style={safeareaviewbg.safeareaviewbg}>
+    <SafeAreaView style={{backgroundColor:'#fff'}}>
       <View style={styles.container}>
         <View style={{}}>
           <Image source={require("../../assets/images/t")} />
